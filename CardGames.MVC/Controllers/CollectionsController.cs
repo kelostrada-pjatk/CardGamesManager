1ca7f8ed0cf8007615441f8308002bd44d77177b
kelostrada <kelostrada@gmail.com>
2016-02-08 10:38:04 +0100

    Collections CRUD implemented

--- CardGames.MVC/Controllers/CollectionsController.cs
+++ CardGames.MVC/Controllers/CollectionsController.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using CardGames.MVC;
 using CardGames.MVC.Models.CardGames;
 using Microsoft.AspNet.Identity;
 
 namespace CardGames.MVC.Controllers
 {
+    [Authorize]
     public class CollectionsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Collections
-        [Authorize]
         public ActionResult Index()
         {
             var userId = User.Identity.GetUserId();
@@ -26,25 +21,24 @@ namespace CardGames.MVC.Controllers
         }
 
         // GET: Collections/Details/5
-        [Authorize]
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Collection collection = db.CardLists.OfType<Collection>().FirstOrDefault(c => c.Id == id);
+            var userId = User.Identity.GetUserId();
+            Collection collection = db.CardLists.OfType<Collection>().FirstOrDefault(c => c.Id == id && c.UserId == userId);
             if (collection == null)
             {
                 return HttpNotFound();
             }
-            return View(collection);
+            return View(CollectionViewModel.FromCollection(collection));
         }
 
         // GET: Collections/Create
         public ActionResult Create()
         {
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Email");
             return View();
         }
 
@@ -53,17 +47,18 @@ namespace CardGames.MVC.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Public,UserId")] Collection collection)
+        public ActionResult Create([Bind(Include = "Id,Name,Public,Deck")] CollectionViewModel collectionViewModel)
         {
             if (ModelState.IsValid)
             {
+                var collection = collectionViewModel.GetCollection();
+                collection.UserId = User.Identity.GetUserId();
                 db.CardLists.Add(collection);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", collection.UserId);
-            return View(collection);
+            return View(collectionViewModel);
         }
 
         // GET: Collections/Edit/5
@@ -73,13 +68,13 @@ namespace CardGames.MVC.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Collection collection = db.CardLists.OfType<Collection>().FirstOrDefault(c => c.Id == id);
+            var userId = User.Identity.GetUserId();
+            Collection collection = db.CardLists.OfType<Collection>().FirstOrDefault(c => c.Id == id && c.UserId == userId);
             if (collection == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", collection.UserId);
-            return View(collection);
+            return View(CollectionViewModel.FromCollection(collection));
         }
 
         // POST: Collections/Edit/5
@@ -87,16 +82,17 @@ namespace CardGames.MVC.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Public,UserId")] Collection collection)
+        public ActionResult Edit([Bind(Include = "Id,Name,Public,Deck")] CollectionViewModel collectionViewModel)
         {
             if (ModelState.IsValid)
             {
+                var collection = collectionViewModel.GetCollection();
+                collection.UserId = User.Identity.GetUserId();
                 db.Entry(collection).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", collection.UserId);
-            return View(collection);
+            return View(collectionViewModel);
         }
 
         // GET: Collections/Delete/5
--- CardGames.MVC/Models/CardGames/CardsViewModels.cs
+++ CardGames.MVC/Models/CardGames/CardsViewModels.cs
@@ -26,4 +26,49 @@ namespace CardGames.MVC.Models.CardGames
         [Required]
         public int EditionId { get; set; }
     }
+
+    public class CollectionViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Is Public?")]
+        public bool Public { get; set; }
+        [Required]
+        [Display(Name = "Is Deck?")]
+        public bool Deck { get; set; }
+
+        public Collection GetCollection()
+        {
+            if (Deck)
+            {
+                return new Deck
+                {
+                    Id = Id,
+                    Name = Name,
+                    Public = Public
+                };
+            }
+            return new Collection
+            {
+                Id = Id,
+                Name = Name,
+                Public = Public
+            };
+        }
+
+        public static CollectionViewModel FromCollection(Collection collection)
+        {
+            return new CollectionViewModel
+            {
+                Id = collection.Id,
+                Name = collection.Name,
+                Public = collection.Public,
+                Deck = collection is Deck
+            };
+        }
+    }
 }
